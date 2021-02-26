c9011203e64385e940bad56ca0fcb76ec11e30df
khaledsallem <70590665+khaledsallem@users.noreply.github.com>
2021-02-26 14:24:24 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import VisibilityFilters from "./components/VisibilityFilters";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <AddTodo />
+      <TodoList />
+      <VisibilityFilters />
+    </div>
+  );
+}
+
+
+
+export default App;
